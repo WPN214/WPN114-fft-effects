a83c949cbd8cd4d68e09c2c081824d53fef7c8b1
pchdev <pchd@quarre.fr>
2019-06-29 13:04:22 +0200

    updated api

--- source/convolver/convolver.cpp
+++ source/convolver/convolver.cpp
@@ -1,61 +1,134 @@
-#include "convolver.hpp"
-#include <QtDebug>
+#ifndef CONVOLVER_HPP
+#define CONVOLVER_HPP
 
-Convolver::Convolver() : m_ir(nullptr), m_convolver_l(nullptr), m_convolver_r(nullptr),
-    m_buffer(nullptr)
+#include <wpn114audio/graph.hpp>
+#include <external/fftconvolver/FFTConvolver.h>
+#include <sndfile.h>
+
+using namespace fftconvolver;
+#define cnv_bsize 1024
+
+//-------------------------------------------------------------------------------------------------
+class Convolver : public Node
+//-------------------------------------------------------------------------------------------------
 {
-    SETN_IN     ( 2 );
-    SETN_OUT    ( 2 );
-    SETTYPE     ( StreamType::Effect );
-}
+    Q_OBJECT
 
-Convolver::~Convolver()
-{
-    delete m_ir;
-    delete m_convolver_l;
-    delete m_convolver_r;
-}
+    WPN_DECLARE_DEFAULT_AUDIO_INPUT     (audio_in, 2)
+    WPN_DECLARE_DEFAULT_AUDIO_OUTPUT    (audio_out, 2)
 
-void Convolver::setIrPath(QString path)
-{
-    m_ir_path = path;
-}
+    Q_PROPERTY (QString irPath READ ir_path WRITE set_ir_path )
 
-void Convolver::componentComplete()
-{
-    m_ir = new Soundfile( m_ir_path );
-    m_convolver_l = new FFTConvolver;
-    m_convolver_r = new FFTConvolver;
+public:
+    //-------------------------------------------------------------------------------------------------
+    Convolver()
+    {
 
-    auto nsamples = m_ir->nsamples();
+    }
+    //-------------------------------------------------------------------------------------------------
+    virtual
+    ~Convolver() override
+    //-------------------------------------------------------------------------------------------------
+    {
+        sf_close(m_file);
 
-    StreamNode::allocateBuffer(m_buffer, m_ir->nchannels(), nsamples);
-    m_ir->buffer(m_buffer, 0, nsamples);
-}
+        delete m_convolver_l;
+        delete m_convolver_r;
 
-void Convolver::initialize(qint64 nsamples)
-{    
-    auto ns = m_ir->nsamples();
+        for (nchannels_t n = 0; n < m_info.channels; ++n)
+            delete[] m_buffer[n];
 
-    m_convolver_l->init( CONVOLVER_BUFFER_SIZE, m_buffer[0], ns );
+        delete[] m_buffer;
+    }
 
-    if      ( m_ir->nchannels() == 1 )
-        m_convolver_r->init( CONVOLVER_BUFFER_SIZE, m_buffer[0], ns );
+    //-------------------------------------------------------------------------------------------------
+    virtual void
+    componentComplete() override
+    //-------------------------------------------------------------------------------------------------
+    {
+        if (m_ir_path.isEmpty())
+            return;
 
-    else if ( m_ir->nchannels() == 2 )
-        m_convolver_r->init( CONVOLVER_BUFFER_SIZE, m_buffer[1], ns );
-}
+        m_file = sf_open(CSTR(m_ir_path), SFM_READ, &m_info);
+        Node::componentComplete();
+    }
 
-float** Convolver::process(float** buf, qint64 nsamples)
-{
-    auto nout   = m_num_outputs;
-    auto out    = m_out;   
+    //-------------------------------------------------------------------------------------------------
+    virtual void
+    initialize(Graph::properties const& properties) override
+    //-------------------------------------------------------------------------------------------------
+    {
+        m_buffer = new float*[m_info.channels]();
 
-    StreamNode::resetBuffer( m_out, nout, nsamples );
+        for (nchannels_t n = 0; n < m_info.channels; ++n)
+             m_buffer[n] = new float[m_info.frames];
 
-    m_convolver_l->process( buf[ 0 ], out[ 0 ], nsamples );
-    m_convolver_r->process( buf[ 1 ], out[ 1 ], nsamples );
+        if (m_info.channels == 1)
+            sf_readf_float(m_file, m_buffer[0], m_info.frames);
 
-    return out;
-}
+        else
+        {
+            // create temporary recipient
+            float* tmp = new float[m_info.frames]();
+            sf_readf_float(m_file, m_buffer[0], m_info.frames);
 
+            // deinterleave buffer
+            for (size_t f = 0; f < m_info.frames; ++f)
+                for (nchannels_t c = 0; c < m_info.channels; ++c)
+                    m_buffer[c][f] = *tmp++;
+
+            delete[] tmp;
+        }
+
+        m_convolver_l = new FFTConvolver;
+        m_convolver_r = new FFTConvolver;
+
+        m_convolver_l->init(cnv_bsize, m_buffer[0], m_info.frames);
+
+        if (m_info.channels == 1)
+             m_convolver_r->init(cnv_bsize, m_buffer[0], m_info.frames);
+        else m_convolver_r->init(cnv_bsize, m_buffer[1], m_info.frames);
+    }
+
+    //-------------------------------------------------------------------------------------------------
+    virtual void
+    rwrite(pool& inputs, pool& outputs, vector_t nframes) override
+    //-------------------------------------------------------------------------------------------------
+    {
+        auto audio_in = inputs.audio[0];
+        auto audio_out = outputs.audio[0];
+
+        m_convolver_l->process(audio_in[0], audio_out[0], nframes);
+        m_convolver_r->process(audio_in[1], audio_out[1], nframes);
+    }
+
+    //-------------------------------------------------------------------------------------------------
+    QString
+    ir_path() const { return m_ir_path; }
+
+    //-------------------------------------------------------------------------------------------------
+    void
+    set_ir_path(QString path) { m_ir_path = path; }
+
+private:
+
+    SF_INFO
+    m_info;
+
+    SNDFILE*
+    m_file = nullptr;
+
+    FFTConvolver*
+    m_convolver_l = nullptr;
+
+    FFTConvolver*
+    m_convolver_r = nullptr;
+
+    float**
+    m_buffer = nullptr;
+
+    QString
+    m_ir_path;
+};
+
+#endif // CONVOLVER_HPP
